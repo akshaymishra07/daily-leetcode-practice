f1eb2d647462e094468ae54cd7004a0406888198
AKSHAY MISHRA <34655368+akshaymishra07@users.noreply.github.com>
2022-04-16 16:56:24 +0530

    Time: 11 ms (41.88%), Space: 47.2 MB (60.91%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,56 +8,27 @@
  * }
  */
 class Solution {
-    
-    static void traverse(TreeNode root, TreeNode p, TreeNode q, List<TreeNode> pathOfP, List<TreeNode> pathOfQ , List<TreeNode> temp ){
-        
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
         if(root == null){
-            return;
+            return root;
         }
         
-        temp.add(root);
-        
-        if(root.val == p.val){
-            
-            for(TreeNode num : temp){
-                pathOfP.add(num);
-            }
-            
+        if(root.val == p.val || root.val == q.val){
+            return root;
         }
         
-        if(root.val == q.val){
-            
-            for(TreeNode num : temp){
-                pathOfQ.add(num);
-            }
-            
+        TreeNode lst = lowestCommonAncestor(root.left,p,q);
+        TreeNode rst = lowestCommonAncestor(root.right,p,q);
+        
+        if(rst == null){
+            return lst;
         }
         
-        traverse(root.left,p,q,pathOfP, pathOfQ,temp);
-        traverse(root.right,p,q,pathOfP, pathOfQ,temp);
-        
-        temp.remove(temp.size() - 1);
-    }
-    
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        List<TreeNode> pathOfP = new ArrayList<>();
-        List<TreeNode> pathOfQ = new ArrayList<>();
-        List<TreeNode> temp = new ArrayList<>();
-        
-        
-        traverse(root,p,q,pathOfP, pathOfQ,temp);
-        
-        int i = 0;
-        
-        while(i < pathOfP.size() && i < pathOfQ.size()){
-            
-            if(pathOfP.get(i) != pathOfQ.get(i)){
-                break;
-            }
-            i++;
-        }    
-    
-        return pathOfP.get(i-1);
+        if(lst == null){
+            return rst;
+        }
+       
+        return root;
     }
 }

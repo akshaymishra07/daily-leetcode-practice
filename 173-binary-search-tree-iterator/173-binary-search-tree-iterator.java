df0f3a940a59400fcd792f3b0f0f6fe466d3018a
AKSHAY MISHRA <34655368+akshaymishra07@users.noreply.github.com>
2022-04-20 23:41:14 +0530

    Time: 38 ms (6.39%), Space: 52.5 MB (5.80%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.java
@@ -15,53 +15,6 @@
  */
 class BSTIterator {
     
-    ArrayList<Integer> list;
-    int i;
-    public BSTIterator(TreeNode root) {
-    
-        list = new ArrayList<>();
-        i = -1;
-        
-        inorder(root, list);
-        
-        System.out.println(list.toString());
-    }
-    
-    static void inorder(TreeNode root, ArrayList<Integer> list){
-        
-        if(root == null){
-            return;
-        }
-        
-        inorder(root.left, list);
-        list.add(root.val);
-        inorder(root.right, list);
-    } 
-    
-    public int next() {
-        
-        if(i < list.size()){
-            return list.get(++i);
-        }
-        
-        return -1;
-    }
-    
-    public boolean hasNext() {
-        
-        return !(i == list.size()-1);
-    }
-}
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator obj = new BSTIterator(root);
- * int param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
- */
-
-/*class BSTIterator {
-    
     TreeNode curr;
     Stack<TreeNode> st;
     public BSTIterator(TreeNode root) {
@@ -73,8 +26,14 @@ class BSTIterator {
     
     public int next() {
         
+        while(curr != null){
+            st.push(curr);
+            curr = curr.left;
+        }
         
-        
+        TreeNode t = st.pop();
+        curr = t.right;
+        return t.val;
         
     }
     
@@ -83,4 +42,12 @@ class BSTIterator {
         return !st.isEmpty() || curr != null;
     }
 }
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
+
+/*
 */

d8501df760b0f74ad69e27fd446e8ecc246464a4
AKSHAY MISHRA <34655368+akshaymishra07@users.noreply.github.com>
2022-05-04 00:19:22 +0530

    Time: 20 ms (52.29%), Space: 71.6 MB (59.10%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,78 @@
+class Pair{
+    
+    int x ;
+    int y ;
+    
+    Pair(int x, int y){
+        
+        this.x = x;
+        this.y = y;
+    }
+    
+}
+
+class Solution {
+    
+    static boolean isValid(int x, int y, int m, int n){
+         
+         return x >= 0 && x < m && y >= 0 && y < n;         
+    }
+    
+    public int[][] updateMatrix(int[][] mat) {
+        
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        boolean[][] visited  = new boolean[m][n];
+        Queue<Pair> q = new LinkedList<>();
+        
+        int[] xmoves = new int[]{1 , -1 , 0 , 0};
+        int[] ymoves = new int[]{0 , 0 , 1 , -1};
+        
+        // adding all the zeroes to the queue  , to start simultaneous bfs
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+               if(mat[i][j] == 0){
+                    q.add(new Pair(i,j));
+                    visited[i][j] = true;
+                }
+            }
+        }
+        
+        int dist = 0;
+         
+        while(!q.isEmpty()){
+            
+            int nums = q.size();
+          
+            while(nums-- > 0){
+                
+                Pair curr = q.poll();
+                
+                for(int k = 0 ; k < xmoves.length; k++){
+                    
+                    int x = curr.x + xmoves[k];
+                    int y = curr.y + ymoves[k];
+                    
+                    if(!isValid(x,y,m,n)){
+                        continue;
+                    }    
+                    
+                    if(!visited[x][y]){
+                            
+                        
+                       visited[x][y] = true;
+                       mat[x][y] = dist + 1;
+                       q.add(new Pair(x,y));
+ 
+                        
+                    }
+                    
+               }
+            }
+            dist++;
+        }  
+        
+        return mat;
+    }
+}

c967323c87c5466e43a55e65e23caf50036630ba
AKSHAY MISHRA <34655368+akshaymishra07@users.noreply.github.com>
2022-07-14 23:39:12 +0530

    Time: 4 ms (64.48%), Space: 44.7 MB (34.53%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,36 +15,29 @@
  */
 class Solution {
     
-    static TreeNode construct(int[] pre, int[] in, int i1 , int j1, int i2 , int j2, Map<Integer, Integer> map){
+    TreeNode construct(int[] inorder, int[] preorder, int i1, int j1,int  i2, int j2, Map<Integer, Integer>map){
         
         if(i1 > j1){
             return null;
         }
-        
-        TreeNode root = new TreeNode(pre[i1]);
-        
-        int i = map.get(pre[i1]);
-        
-        int numsLeft = i - i2;
-        
-        root.left = construct(pre, in, i1+1, i1+numsLeft, i2 , i-1,map);
-        root.right = construct(pre, in, i1+numsLeft+1, j1, i+1 , j2,map);
-        
+        TreeNode root = new TreeNode(preorder[i1]);
+        int i = map.get(preorder[i1]);
+                                                   //pre           //in
+        root.left  = construct(inorder, preorder, i1+1, i1+i-i2,  i2, i-1, map);
+        root.right = construct(inorder, preorder, i1+i-i2+1, j1,  i+1, j2, map);
+     
         return root;
-        
     }
     
-    public TreeNode buildTree(int[] pre, int[] in) {
-        
-        
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < in.length; i++){
-            map.put(in[i] , i);
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> map  = new HashMap<>();
+      
+        for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
         }
+          
+        TreeNode root = construct(inorder, preorder, 0, preorder.length-1, 0, inorder.length-1, map);
         
-        TreeNode ans = construct(pre,in,0,pre.length-1,0,in.length-1,map);
-        
-        return ans;
+        return root;
     }
 }

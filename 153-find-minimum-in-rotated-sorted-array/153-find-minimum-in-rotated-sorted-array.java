3804541475d9163c4d1ed6f8bce54560e7ed6daf
AKSHAY MISHRA <34655368+akshaymishra07@users.noreply.github.com>
2022-07-31 02:12:22 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (25.09%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -14,6 +14,7 @@ class Solution {
                 
                 if(nums[mid] > nums[mid+1]){
                     pivot = mid;
+                    break;
                 }
                 low = mid + 1;
             }

3aca32de2c4267ec320609c85c3fd18bbada4c92
AKSHAY MISHRA <34655368+akshaymishra07@users.noreply.github.com>
2022-10-31 22:16:28 +0530

    Time: 24 ms (11.26%), Space: 43.6 MB (8.26%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+     
+     Map<Character, List<Integer>> map = new HashMap<>();
+     
+     for(int i = 0; i < s.length(); i++){
+         char ch = s.charAt(i);
+         if(!map.containsKey(ch)){
+             List<Integer> temp = new ArrayList<>(2);
+             temp.add(i);
+             temp.add(-1);
+             map.put(ch, temp);
+         }else{
+             List<Integer> temp = map.get(ch);
+             temp.set(1, i);
+         }
+     }
+        
+     List<Integer> ans = new ArrayList<>(); 
+        
+     for(int i = 0; i < s.length(); i++){
+         char ch = s.charAt(i);
+           
+         if(map.get(ch).get(1) == -1){
+             ans.add(1);
+             continue;         
+         }
+         int end = i;   
+         for(int j = i; j <= end; j++){
+            ch = s.charAt(j); 
+            end = Math.max(end, map.get(ch).get(1)); 
+         }
+         
+         ans.add(end-i+1);
+         i = end;
+     }
+       
+     return ans;    
+  }
+}
